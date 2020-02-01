0e8edec846a5e89632063f3531395b89dd3a61cb
ltsaur <48870997+ltsaur@users.noreply.github.com>
2020-02-01 13:40:20 -0800

    remove unnecessary sequential logic

--- dispString.v
+++ dispString.v
@@ -38,7 +38,7 @@
 //
 module dispString(
 		  output reg 	   rdy,
-		  output reg [7:0] dOut,
+		  output wire [7:0] dOut,
 		  input wire [7:0] b0,
 		  input wire [7:0] b1,
 		  input wire [7:0] b2,
@@ -52,21 +52,20 @@ module dispString(
 		  input wire 	   clk);
 
    reg [3:0] 			   cnt;
-   wire [7:0] 			   dOutP;
+   
    always @(posedge clk) begin
-      if (rst)
-	      cnt <= 4'b0000;
-      else begin
-	 if (go | (cnt != 4'b0000))
+    if (rst)
+	   cnt <= 4'b0000;
+    else begin
+	if (go | (cnt != 4'b0000))
 	   cnt <= cnt + 1;
-	 else
+	else
 	   cnt <= cnt;
-      end
-      dOut <= dOutP;
-      rdy <= cnt[0];
+    end
+    rdy <= cnt[0];
    end
 
-   assign dOutP = 
+   assign dOut = 
 		  (cnt[3:1]==3'b000) ? b0 :
 		  (cnt[3:1]==3'b001) ? b1 :
 		  (cnt[3:1]==3'b010) ? b2 :
@@ -78,7 +77,3 @@ module dispString(
    
    
 endmodule // dispString
-
-      
-	
-      
