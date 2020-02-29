f656a95099607787b6f3fc87e4fd8e096fd90919
Steven Wu <steven774@gmail.com>
2020-02-28 18:01:54 -0800

    h

--- digclockdp.v
+++ digclockdp.v
@@ -27,19 +27,35 @@ module didp (
 
     //(dp.1) change this line and add code to set 3 more wires: StensIs5, MonesIs9, MtensIs5
     //   these 4 wires determine if digit reaches 5 or 9.  10% of points assigned to Lab3
-    wire SonesIs9 = (di_iSones == 4'd9);
+    wire SonesIs9 = (di_iSones[3] & !di_iSones[2] & !di_iSones[1] & di_iSones[0]);
+    wire StensIs5 = (!di_iSones[3] & di_iSones[2] & !di_iSones[1] & di_iSones[0]);
+    wire MonesIs9 = (di_iSones[3] & !di_iSones[2] & !di_iSones[1] & di_iSones[0]);
+    wire MtensIs5 = (!di_iSones[3] & di_iSones[2] & !di_iSones[1] & di_iSones[0]);
+//    wire SonesIs9 = (di_iSones == 4'd9);
+//    wire StensIs5 = (di_iStens == 4'd5);
+//    wire MonesIs9 = (di_iMones == 4'd9);
+//    wire MtensIs5 = (di_iMtens == 4'd5);
 
     //(dp.2) add code to set 3 more wires: rollStens, rollMones, rollMtens
     //   these 4 wires determine if digit shall be rolled back to 0 : 10% of points assigned to Lab3
     wire rollSones = SonesIs9;
+    wire rollStens = StensIs5;
+    wire rollMones = MonesIs9;
+    wire rollMtens = MtensIs5;
 
     //(dp.3) add code to set 3 more wires: countEnStens, countEnMones, countEnMtens
     //   these 4 wires generate a strobe to advance counter: 10% of points assigned to Lab3
     wire countEnSones = clkSecStrb; // enable the counter Sones
+    wire countEnStens = clkSecStrb & rollSones;
+    wire countEnMones = clkSecStrb & rollStens;
+    wire countEnMtens = clkSecStrb & rollMones;
  
     //(dp.4) add code to set sTensDin, mOnesDin, mTensDin
     //   0% of points assigned to Lab3, used in Lab4
     wire [3:0] sOnesDin = ldSones ? ld_num : 4'b0;
+    wire [3:0] sTensDin = ldStens ? ld_num : 4'b0;
+    wire [3:0] mOnesDin = ldMones ? ld_num : 4'b0;
+    wire [3:0] mTensDin = ldMtens ? ld_num : 4'b0;
    		
     //(dp.5) add code to generate digital clock output: di_iStens, di_iMones di_iMtens 
     //   20% of points assigned to Lab3
